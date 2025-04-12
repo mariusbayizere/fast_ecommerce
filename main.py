b61211ef26e7aa386a639c0b7e3bcba28e32c8ba
Bayizere Marius <bayizeremarius119@gmail.com>
2025-04-12 08:16:29 +0200

    Updated code

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
+from typing import Optional
+
 
 app =FastAPI()
 
 
 @app.get('/')
 async def read_root():
+    """
+    This is the root function that returns a welcome message.
+    """
     return {"Message":"Hello Marius"}
 
 
 
 @app.get('/greet/{name}')
 async def greeting(name: str, age : int)-> dict:
+    """
+    This is a greeting function that takes a name and age as parameters.
+    It returns a dictionary with the name and age.
+    """
     return {"Message":f"my name is {name} I have ", "Age" : age}
+
+
+@app.get('/greets')
+async def greeting_name(name: Optional[str]="User", age : Optional[int]=20)-> dict:
+    """"
+    This is a greeting function that takes a name and age as parameters.
+    It returns a dictionary with the name and age.
+    """
+    return {"Message":f"My name is {name}", "Age":age}
